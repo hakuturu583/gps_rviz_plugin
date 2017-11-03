7a201068986fa9ad4668ff24d4a327e87f656aa1
MilkWithCoffee <33390210+MilkWithCoffee@users.noreply.github.com>
2017-11-04 01:19:47 +0900

    add google static map api key param

--- src/overlay_gps_display.cpp
+++ src/overlay_gps_display.cpp
@@ -1,9 +1,10 @@
-#include "gps_display.h"
+#include "overlay_gps_display.h"
 
 namespace gps_rviz_plugin
 {
-  gps_display::gps_display()
+  OverlayGpsDisplay::OverlayGpsDisplay()
   {
+    ros::param::get("/overlay_gps_display/google_static_map_api_key" ,api_key_);
     map_image_path_ = ros::package::getPath("gps_rviz_plugin") + "/data/map.png";
     load_map_downloader_script();
     zoom_property_ = new rviz::IntProperty("Zoom Property", 19, "zoom of map", this, SLOT(updateGooleMapAPIProperty()));
@@ -18,7 +19,7 @@ namespace gps_rviz_plugin
     scale_property_ = new rviz::IntProperty("Scale", 1, "request map image scale", this, SLOT(updateGooleMapAPIProperty()));
     scale_property_->setMax(2);
     scale_property_->setMin(1);
-    api_key_property_ = new rviz::StringProperty("API Key", "", "Google Static Map API Key", this, SLOT(updateGooleMapAPIProperty()));
+    //api_key_property_ = new rviz::StringProperty("API Key", "", "Google Static Map API Key", this, SLOT(updateGooleMapAPIProperty()));
     maptype_property_ = new rviz::EnumProperty("Map Type", "roadmap", "map type", this, SLOT(updateGooleMapAPIProperty()));
     maptype_property_->addOption("roadmap", ROADMAP);
     maptype_property_->addOption("terrain", TERRAIN);
@@ -33,18 +34,18 @@ namespace gps_rviz_plugin
     messages_per_plot_property_->setMin(1);
   }
 
-  gps_display::~gps_display()
+  OverlayGpsDisplay::~OverlayGpsDisplay()
   {
     delete zoom_property_;
     delete width_property_;
     delete height_property_;
     delete scale_property_;
     delete alpha_property_;
-    delete api_key_property_;
+    //delete api_key_property_;
     delete maptype_property_;
   }
 
-  void gps_display::download_map(std::string request_url)
+  void OverlayGpsDisplay::download_map(std::string request_url)
   {
     PyObject* args = PyTuple_New(1);
     PyObject* kw_args = PyDict_New();
@@ -55,72 +56,75 @@ namespace gps_rviz_plugin
     Py_DECREF(kw_args);
   }
 
-  void gps_display::onInitialize()
+  void OverlayGpsDisplay::onInitialize()
   {
     MFDClass::onInitialize();
   }
 
-  void gps_display::reset()
+  void OverlayGpsDisplay::reset()
   {
     MFDClass::reset();
   }
 
-  void gps_display::processMessage(const sensor_msgs::NavSatFix::ConstPtr& msg)
+  void OverlayGpsDisplay::processMessage(const sensor_msgs::NavSatFix::ConstPtr& msg)
   {
     if(msg->header.seq%messages_per_plot_property_->getInt()==0)
     {
+      std::string request_url;
+      if(build_request_url(msg, request_url) == false)
+      {
+        return;
+      }
       try
       {
-        std::string request_url;
-        build_request_url(msg, request_url);
         download_map(request_url);
-        cv::Mat map_image = cv::imread(map_image_path_);
-        if(!overlay_)
-        {
-          static int count = 0;
-          rviz::UniformStringStream ss;
-          ss << "OverlayImageDisplayObject" << count++;
-          overlay_.reset(new OverlayObject(ss.str()));
-          overlay_->show();
-        }
-        if (overlay_)
-        {
-          overlay_->setDimensions(width_property_->getInt(), height_property_->getInt());
-          overlay_->setPosition(position_x_property_->getInt(),position_y_property_->getInt());
-        }
-        overlay_->updateTextureSize(width_property_->getInt(),height_property_->getInt());
-        ScopedPixelBuffer buffer = overlay_->getBuffer();
-        QImage Hud = buffer.getQImage(*overlay_);
-        for (int i = 0; i < overlay_->getTextureWidth(); i++)
-        {
-          for (int j = 0; j < overlay_->getTextureHeight(); j++)
-          {
-            QColor color(map_image.data[j * map_image.step + i * map_image.elemSize() + 2],
-                         map_image.data[j * map_image.step + i * map_image.elemSize() + 1],
-                         map_image.data[j * map_image.step + i * map_image.elemSize() + 0],
-                         alpha_property_->getFloat() * 255.0);
-            Hud.setPixel(i, j, color.rgba());
-          }
-        }
       }
       catch(...)
       {
         ROS_ERROR_STREAM("failed to request map");
       }
+      cv::Mat map_image = cv::imread(map_image_path_);
+      if(!overlay_)
+      {
+        static int count = 0;
+        rviz::UniformStringStream ss;
+        ss << "OverlayGpsDisplayObject" << count++;
+        overlay_.reset(new OverlayObject(ss.str()));
+        overlay_->show();
+      }
+      if (overlay_)
+      {
+        overlay_->setDimensions(width_property_->getInt(), height_property_->getInt());
+        overlay_->setPosition(position_x_property_->getInt(),position_y_property_->getInt());
+      }
+      overlay_->updateTextureSize(width_property_->getInt(),height_property_->getInt());
+      ScopedPixelBuffer buffer = overlay_->getBuffer();
+      QImage Hud = buffer.getQImage(*overlay_);
+      for (int i = 0; i < overlay_->getTextureWidth(); i++)
+      {
+        for (int j = 0; j < overlay_->getTextureHeight(); j++)
+        {
+          QColor color(map_image.data[j * map_image.step + i * map_image.elemSize() + 2],
+                       map_image.data[j * map_image.step + i * map_image.elemSize() + 1],
+                       map_image.data[j * map_image.step + i * map_image.elemSize() + 0],
+                       alpha_property_->getFloat() * 255.0);
+          Hud.setPixel(i, j, color.rgba());
+        }
+      }
     }
   }
 
-  void gps_display::updateGooleMapAPIProperty()
+  void OverlayGpsDisplay::updateGooleMapAPIProperty()
   {
 
   }
 
-  void gps_display::updateDisplayProperty()
+  void OverlayGpsDisplay::updateDisplayProperty()
   {
 
   }
 
-  void gps_display::load_map_downloader_script()
+  void OverlayGpsDisplay::load_map_downloader_script()
   {
     std::string map_downloader_path = ros::package::getPath("gps_rviz_plugin") + "/scripts/map_downloader.py";
     std::ifstream ifs(map_downloader_path.c_str());
@@ -139,15 +143,17 @@ namespace gps_rviz_plugin
     map_downloader_function_ = PyDict_GetItemString(script_obj, "download_map");
   }
 
-  bool gps_display::build_request_url(const sensor_msgs::NavSatFix::ConstPtr& msg, std::string& request_url)
+  bool OverlayGpsDisplay::build_request_url(const sensor_msgs::NavSatFix::ConstPtr& msg, std::string& request_url)
   {
     request_url = "https://maps.googleapis.com/maps/api/staticmap?";
+    /*
     if(api_key_property_->getStdString() == "")
     {
       this->setStatus(rviz::StatusProperty::Level::Error, "APIKey", "API key is not exist");
       return false;
     }
-    this->setStatus(rviz::StatusProperty::Level::Ok, "APIKey", "API key is exist");
+    */
+    //this->setStatus(rviz::StatusProperty::Level::Ok, "APIKey", "API key is exist");
     std::string center_request = "center=" + std::to_string(msg->latitude) + "," + std::to_string(msg->longitude);
     request_url = request_url + center_request;
     std::string markers_request = "&markers=color:red%7C" + std::to_string(msg->latitude) + "," + std::to_string(msg->longitude);
@@ -177,7 +183,7 @@ namespace gps_rviz_plugin
       request_url = request_url + maptype_url;
     }
 
-    std::string key_request = "&key=" + api_key_property_->getStdString();
+    std::string key_request = "&key=" + api_key_;//api_key_property_->getStdString();
     request_url = request_url + key_request;
     if(request_url.size() > MAX_REQUEST_URL_LENGTH)
     {
@@ -195,4 +201,4 @@ namespace gps_rviz_plugin
 }
 
 #include <pluginlib/class_list_macros.h>
-PLUGINLIB_EXPORT_CLASS(gps_rviz_plugin::gps_display, rviz::Display)
+PLUGINLIB_EXPORT_CLASS(gps_rviz_plugin::OverlayGpsDisplay, rviz::Display)
